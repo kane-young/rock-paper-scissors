8af63ad13e710e7b88c7a8006821d88fb7f470ea
kane-young <lyw2100@naver.com>
2021-09-12 16:10:33 +0900

    Chore: RockPaperScissorGame 내부에 정의되어 있던 타입들 밖으로 이동

--- RockPaperScissors/RockPaperScissors/RockPaperScissorsGame.swift
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsGame.swift
@@ -5,32 +5,32 @@
 //  Created by 강경 on 2021/03/05.
 //
 
-public class RockPaperScissorsGame {
-    enum gamePlayer: String {
-        case player = "플레이어"
-        case computer = "컴퓨터"
-    }
-    
-    enum Hand: String, CaseIterable, Comparable {
-        static func < (lhs: Hand, rhs: Hand) -> Bool {
-            if (lhs == .scissors && rhs == .rock)
+enum gamePlayer: String {
+    case player = "플레이어"
+    case computer = "컴퓨터"
+}
+
+enum Hand: String, CaseIterable, Comparable {
+    static func < (lhs: Hand, rhs: Hand) -> Bool {
+        if (lhs == .scissors && rhs == .rock)
             || (lhs == .paper && rhs == .scissors)
             || (lhs == .rock && rhs == .paper) {
-                return true
-            } else {
-                return false
-            }
+            return true
+        } else {
+            return false
         }
-        
-        case scissors = "1"
-        case rock = "2"
-        case paper = "3"
     }
     
-    enum GameError: Error {
-        case invalidInput
-    }
-    
+    case scissors = "1"
+    case rock = "2"
+    case paper = "3"
+}
+
+enum GameError: Error {
+    case invalidInput
+}
+
+public class RockPaperScissorsGame {
     func startGame() {
         var isRepeat = false
         
@@ -38,7 +38,7 @@ public class RockPaperScissorsGame {
             guard let computersHand = Hand.allCases.randomElement() else {
                 continue
             }
-            
+
             var playerHand: Hand
             do {
                 if let notNilplayerHand = try getHandByPlayer() {
@@ -56,7 +56,7 @@ public class RockPaperScissorsGame {
             isRepeat = gameResult(playerHand, vs: computersHand)
         } while isRepeat
     }
-    
+
     func gameResult(_ playersHand: Hand, vs computersHand: Hand) -> Bool {
         if playersHand == computersHand {
             print("비겼습니다.")
@@ -71,7 +71,7 @@ public class RockPaperScissorsGame {
             return false
         }
     }
-        
+
     func getHandByPlayer() throws -> Hand? {
         print("가위(1), 바위(2), 보(3)! <종료 : 0> : ", terminator: "")
         
